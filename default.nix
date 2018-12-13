07e5403942042eba1d922aeff2e8ce8610e9536f
grumply <sean@grump.ly>
2018-12-12 18:17:37 -0600

    Extend the parsing functionality to allow user-supplied language pragmas.

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
-{ mkDerivation, base, haskell-src-meta, interpolate, stdenv }:
+{ mkDerivation, base, haskell-src-exts, haskell-src-meta, pure-txt, pure-txt-interpolate, stdenv }:
 mkDerivation {
   pname = "pure-template";
   version = "0.7.0.0";
   src = ./.;
-  libraryHaskellDepends = [ haskell-src-meta interpolate ];
+  libraryHaskellDepends = [ base haskell-src-exts haskell-src-meta pure-txt pure-txt-interpolate ];
   homepage = "github.com/grumply/pure-template";
   description = "String interpolating template macros";
   license = stdenv.lib.licenses.bsd3;
--- src/Pure/Template.hs
+++ src/Pure/Template.hs
@@ -1,14 +1,20 @@
 {-# LANGUAGE FlexibleInstances #-}
-module Pure.Template (module Pure.Template, i) where
+module Pure.Template (module Pure.Template, i, Q, Dec, parseModeWithExtensions, defaultExtensions, parseDecls, ToTxt(..), FromTxt(..), parse) where
 
-import Data.String.Interpolate
+import Pure.Data.Txt
+import Pure.Data.Txt.Interpolate
+
+import Language.Haskell.Exts.Extension
+import Language.Haskell.Exts.Parser hiding (parse)
+import Language.Haskell.Exts.SrcLoc
+import Language.Haskell.Exts.Syntax
 
 import Language.Haskell.Meta
 
-import Language.Haskell.TH
-import Language.Haskell.TH.Quote
+import Language.Haskell.TH (Q,Dec)
 
 import Data.String
+import Data.List as List
 
 -- | This module plumbs together haskell-src-meta/template-haskell with string 
 -- interpolation to enable interpolating haskell source templates. This module 
@@ -32,5 +38,32 @@ import Data.String
 --       into a use-site module.  This closed trac ticket suggests it is 
 --       not possible: https://ghc.haskell.org/trac/ghc/ticket/1475
 
+parseModeWithExtensions :: [KnownExtension] -> ParseMode
+parseModeWithExtensions exts = ParseMode [] Haskell2010 (EnableExtension <$> exts) False False Nothing False
+
+defaultExtensions :: [KnownExtension]
+defaultExtensions = 
+    [QuasiQuotes
+    ,TemplateHaskell
+    ,RankNTypes
+    ,MultiParamTypeClasses
+    ,FlexibleContexts
+    ,ViewPatterns
+    ,PatternSynonyms
+    ,TypeFamilies
+    ,TypeOperators
+    ]
+
+parseDecls :: [KnownExtension] -> String -> Either String [Decl SrcSpanInfo]
+parseDecls exts = either Left (Right . moduleDecls) 
+                . parseResultToEither 
+                . parseModuleWithMode (parseModeWithExtensions exts)
+
+parse :: [KnownExtension] -> Txt -> Q [Dec]
+parse exts = either fail (return . List.concatMap toDecs) . parseDecls exts . fromTxt
+
+instance FromTxt (Q [Dec]) where
+    fromTxt = Pure.Template.parse defaultExtensions
+
 instance IsString (Q [Dec]) where
-    fromString = either fail (return . concatMap toDecs) . parseHsDecls
+    fromString = Pure.Template.parse defaultExtensions . toTxt
