9a3f7210a0911e2db4c91017432033ccb0ecd878
Sean Hickman <sean@grumply.com>
2020-02-13 23:36:40 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, haskell-src-exts, haskell-src-meta, pure-txt, pure-txt-interpolate, stdenv }:
 mkDerivation {
   pname = "pure-template";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base haskell-src-exts haskell-src-meta pure-txt pure-txt-interpolate ];
   homepage = "github.com/grumply/pure-template";
